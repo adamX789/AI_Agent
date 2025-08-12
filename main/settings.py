33f535a6ec9734467888aa6739e120050d2f10eb
Adam Sorf <adamsorf99@gmail.com>
2025-08-12 19:49:27 +0200

    Uprava settings.py.

--- main/settings.py
+++ main/settings.py
@@ -24,16 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", cast=str)
 
-DATABASE_URL = config("DATABASE_URL")
+DATABASE_URL = config("DATABASE_URL", cast=str)
 
-GOOGLE_API_KEY = config("GOOGLE_API_KEY")
+GOOGLE_API_KEY = config("GOOGLE_API_KEY", cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", ""]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+                 os.environ.get('RAILWAY_STATIC_URL', '')]
 
 
 # Application definition
