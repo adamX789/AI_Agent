0c988b8b3111227b1cbcf5a0ec72fbc0ed97d4f0
Adam Sorf <adamsorf99@gmail.com>
2025-08-15 16:36:24 +0200

    Kompletní úprava chatbota

--- chat/main.py
+++ chat/main.py
@@ -1,58 +1,23 @@
+import pathlib
+import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent, AgentExecutor
-from .tools import tools
+from google import genai
+from google.genai import types
 
 load_dotenv()
 
 def chatbot(query):
-    class ResearchResponse(BaseModel):
-        summary: str
-
-
-    llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
-    parser = PydanticOutputParser(pydantic_object=ResearchResponse)
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-                Jsi fitness trenér, který se specializuje na výživu.
-                Odpověz na dotaz uživatele a použij k tomu POUZE informace z přiloženého textu pomocí nástroje Najit_informace_z_textu, žádné jiné zdroje.
-                **PRAVIDLA PRO VÝSTUP:**
-                -Pokud dotaz nesouvisí s tématem výživy, odpověz do pole "summary" hláškou: "Omlouvám se, na tuto otázku neumím odpovědět, protože se specializuji pouze na výživu.".
-                -Pokud nástroj Najit_informace_z_textu vrátí prázdnou odpověď, napiš do pole "summary" hlášku: "Omlouvám se, na tuto otázku neumím pomocí přiloženého textu odpovědět.".
-                -Ve všech ostatních případech napiš do pole "summary" odpověď z nástroje Najit_informace_z_textu.
-                -Text rozděl do odstavců, aby byl lépe čitelný.
-                -Výstup zabal do tohoto formátu a neuváděj žádný jiný text\n{format_instructions}.
-                """,
-            ),
-            ("placeholder", "{chat_history}"),
-            ("human", "{query}"),
-            ("placeholder", "{agent_scratchpad}"),
-        ]
-    ).partial(format_instructions=parser.get_format_instructions())
-
-
-    agent = create_tool_calling_agent(
-        llm=llm,
-        prompt=prompt,
-        tools=tools
+    client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+    file = pathlib.Path("chat/vyzivovy_specialista.pdf")
+    contents = [types.Part.from_bytes(data=file.read_bytes(),mime_type="application/pdf"), query]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=contents,
     )
+    return response.text
 
-    agent_executor = AgentExecutor(
-        agent=agent, tools=tools, verbose=True)
-    raw_response = agent_executor.invoke(
-        {"query": query})
 
-    try:
-        output_string = raw_response.get(
-            "output").strip('`json').strip('`').strip()
-        structured_response = parser.parse(output_string)
-        return f"{structured_response.summary}"
-    except Exception as e:
-        return f"Problém s parsováním odpovědi: {e}\nNeparsovaná odpověď:{raw_response}"
+
+
+    
