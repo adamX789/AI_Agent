011d75ec943ebc6c96b94ffb651f1bcbbc0ed498
Adam Sorf <adamsorf99@gmail.com>
2025-08-16 13:56:28 +0200

    Úprava agenta

--- chat/main.py
+++ chat/main.py
@@ -10,12 +10,14 @@ load_dotenv()
 def chatbot(query):
     client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
     file = pathlib.Path("chat/vyzivovy_specialista.pdf")
-    instrukce = "Odpověz na následující dotaz pouze pomocí přiloženého dokumentu. Pokud v dokumentu nenajdeš odpověď, napiš, že jsi v dokumentu nenašel odpověď na danou otázku"
-    dotaz = f"{instrukce}\n\n{query}"
-    contents = [types.Part.from_bytes(data=file.read_bytes(),mime_type="application/pdf"), dotaz]
+    config = types.GenerateContentConfig(
+        system_instruction="Odpověz na následující dotaz pouze pomocí přiloženého dokumentu. Pokud v dokumentu nenajdeš odpověď, napiš, že jsi v dokumentu nenašel odpověď na danou otázku"
+    )
+    contents = [types.Part.from_bytes(data=file.read_bytes(),mime_type="application/pdf"), query]
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=contents,
+        config=config
     )
     return response.text
 
