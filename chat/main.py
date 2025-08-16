993398f33686f29f4c3fa3e43d8479ce75e8feb0
Adam Sorf <adamsorf99@gmail.com>
2025-08-16 13:44:21 +0200

    Nové pdfko

--- chat/main.py
+++ chat/main.py
@@ -10,7 +10,9 @@ load_dotenv()
 def chatbot(query):
     client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
     file = pathlib.Path("chat/vyzivovy_specialista.pdf")
-    contents = [types.Part.from_bytes(data=file.read_bytes(),mime_type="application/pdf"), query]
+    instrukce = "Odpověz na následující dotaz pouze pomocí přiloženého dokumentu. Pokud v dokumentu nenajdeš odpověď, napiš, že jsi v dokumentu nenašel odpověď na danou otázku"
+    dotaz = f"{instrukce}\n\n{query}"
+    contents = [types.Part.from_bytes(data=file.read_bytes(),mime_type="application/pdf"), dotaz]
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=contents,
