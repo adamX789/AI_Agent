f8abba7703760c7bfcd02395d348f5631c119dca
Adam Sorf <adamsorf99@gmail.com>
2025-09-06 08:25:30 +0200

    Úprava souboru pro resetování jídla

--- clear_daily_food.py
+++ clear_daily_food.py
@@ -5,7 +5,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 django.setup()
 
 from chat.models import Message
-from user_profile.models import Food
+from user_profile.models import Food,Profile
 from django.contrib.auth import get_user_model
 from muj_den.funkce import sestav_jidelnicek
 
@@ -16,7 +16,7 @@ def reset_daily_food():
     users = User.objects.all()
     message_text = "Ahoj, na co máš dneska chuť, nebo co ti zbylo v lednici?😋 Zkusím ti podle toho sestavit jídelníček na základě tvých makroživin. Pokud nic nenapíšeš, vymyslím něco podle sebe."
     for user in users:
-        profile = user.profile
+        profile = Profile.objects.get(user=user)
         sestav_jidelnicek(profile=profile,reset=True)
         Message.objects.create(
             user=user,
